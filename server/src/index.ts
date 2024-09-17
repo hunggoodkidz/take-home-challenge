183ad1c2fc3038c3d11ef51a99b3f9937ae777a7
hunggoodkidz <ninbook0708@gmail.com>
2024-09-17 18:34:56 +0700

    added swagger for API Documentation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import { swaggerUi, swaggerDocument } from './swagger'; // Adjust the path if necessary
 
 import deviceRoutes from './routes/deviceRoutes'; // Import device routes
 import nodeRoutes from './routes/nodeRoutes'; // Import node routes
@@ -9,6 +9,7 @@ import linkRoutes from './routes/linkRoutes'; // Import link routes
 import dataPointRoutes from './routes/dataPointRoutes'; // Import data point routes
 import notificationRoutes from './routes/notificationRoutes'; //Import notification routes
 import pipelineRoutes from './routes/pipelineRoutes'; //Import pipeline routes
+
 dotenv.config();
 
 const app = express();
@@ -17,6 +18,9 @@ const port = process.env.PORT || 3000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+// Swagger route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/api/devices', deviceRoutes); // Device routes
